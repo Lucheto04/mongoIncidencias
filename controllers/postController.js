aab8c27118011b718033ef5332b33c89a8ff2d23
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-23 17:18:12 -0500

    refactor: :ambulance: Quitamos la ruta del error a mostrar
    Se puso la ruta para testear y solucionar errores, ya se completo por lo que se deja la base de 'error' y ya

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,6 +6,6 @@ export const postTrainerController = async (req, res) => {
         const result = await postService.postTrainerService(data);
         res.status(201).send(result)
     } catch (error) {
-        res.status(500).send(error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied);
+        res.status(500).send(error);
     }
 }
