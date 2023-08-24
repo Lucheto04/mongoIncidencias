cb73b711db2f8ca67474250be88701c2b60b7cc3
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-24 06:06:06 -0500

    feat: :sparkles: Importamos el putInitRouter para poder manejar el endpoint de put en trainers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import getInitRoute from "./getRoute.js";
-import postInitRoute from "./postRoute.js"
+import postInitRoute from "./postRoute.js";
+import putInitRouter from "./putRoute.js";
 
 const initApiRoute = () => {
     const apiRouter = Router();
     apiRouter.use('/get', getInitRoute());
     apiRouter.use('/post', postInitRoute());
+    apiRouter.use('/put', putInitRouter())
     return apiRouter;
 }
 
