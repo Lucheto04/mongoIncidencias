79ac1c52987a21c0db595c2889845ea80e18da37
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-24 09:03:04 -0500

    feat: :sparkles: Creamos la verificacion del token
    Añadimos el middleware de verificacion en todas las consultas

--- middleware/token.js
+++ middleware/token.js
@@ -27,3 +27,20 @@ export const tokenGeneretor = async (req, res) =>{
         res.status(404).send({status: 404, message: 'Token solicitado no existente'})
     }   
 }
+
+export const tokenVerify = async (req, res, next) => {
+    const {authorization} = req.headers;
+    if (!authorization) return res.status(400).send({status: 400, token: "Token no enviado"});
+    try {
+        const encoder = new TextEncoder();
+        const jwtData = await jwtVerify(
+            authorization,
+            encoder.encode(process.env.JWT_PRIVATE_KEY)
+        );
+        req.data = jwtData;
+        console.log(jwtData);
+        next();
+    } catch (error) {
+        res.status(498).send({status: 498, token: "Token caducado"});
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ import getInitRoute from "./getRoute.js";
 import postInitRoute from "./postRoute.js";
 import putInitRouter from "./putRoute.js";
 import deleteInitRoute from "./deleteRouter.js";
-import { tokenGeneretor } from "../middleware/token.js";
+import { tokenGeneretor, tokenVerify } from "../middleware/token.js";
 
 const initApiRoute = () => {
     const apiRouter = Router();
     apiRouter.use('/token/:id', tokenGeneretor)
-    apiRouter.use('/get', getInitRoute());
-    apiRouter.use('/post', postInitRoute());
-    apiRouter.use('/put', putInitRouter());
-    apiRouter.use('/delete', deleteInitRoute());
+    apiRouter.use('/get', tokenVerify, getInitRoute());
+    apiRouter.use('/post', tokenVerify, postInitRoute());
+    apiRouter.use('/put', tokenVerify, putInitRouter());
+    apiRouter.use('/delete', tokenVerify, deleteInitRoute());
     return apiRouter;
 }
 
