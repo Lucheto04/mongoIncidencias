5a56dddab26e52063fc290cb7cbcb1e01bcdcd1d
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-24 16:37:19 -0500

    refactor: :ambulance: Cambiamos las variables y el objeto que le pasamos al token

--- middleware/deleteIncidencia.middle.js
+++ middleware/deleteIncidencia.middle.js
@@ -1,16 +1,16 @@
 import { collectionGen } from "../db/atlas.js";
 
-export const deleteMiddleware = async (req, res, next) => {
+export const deleteIncidenciaMiddleware = async (req, res, next) => {
     try {
         let {payload} = req.data;
         const {iat, exp, ...newPayload } = payload
         payload = newPayload
         const connection = await collectionGen('trainer');
-            const trainer = await connection.find(
-                {
-                    id_trainer: 1
-                }
-            ).toArray()
+        const trainer = await connection.find(
+            {
+                id_trainer: 1
+            }
+        ).toArray()
         const {_id} = trainer[0];
         let verify = _id === JSON.stringify(payload);
         next();
--- middleware/token.js
+++ middleware/token.js
@@ -6,7 +6,6 @@ export const tokenGeneretor = async (req, res) =>{
     try {
         const {id} = req.params
         const id_numero = parseInt(id)
-        console.log(id_numero);
         const connection = await collectionGen('trainer');
         const trainer = await connection.find(
             {
@@ -15,8 +14,9 @@ export const tokenGeneretor = async (req, res) =>{
         ).toArray()
         if (!trainer) res.status(404).send("usuario no existente");
         const {_id} = trainer[0];
+        const id_string = _id.toString()
         const encoder = new TextEncoder();
-        const jwtconstructor = new SignJWT({_id});
+        const jwtconstructor = new SignJWT({id: id_string});
         const jwt = await jwtconstructor
         .setProtectedHeader({alg:"HS256", typ: "JWT"})
         .setIssuedAt()
@@ -24,6 +24,7 @@ export const tokenGeneretor = async (req, res) =>{
         .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
         res.status(201).send({status: 201, message: jwt});
     } catch (error) {
+        console.log(error);
         res.status(404).send({status: 404, message: 'Token solicitado no existente'})
     }   
 }
