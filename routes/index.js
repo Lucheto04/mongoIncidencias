1ce33aea7eb30173ed510780224b8e6be0201bb1
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-25 06:19:23 -0500

    refactor: :fire: Eliminamos el middleware de deleteIncidencia

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,13 @@ import putInitRouter from "./putRoute.js";
 import deleteInitRoute from "./deleteRouter.js";
 import { limitPets, limitSize } from "../helpers/limit.js";
 import { tokenGeneretor, tokenVerify } from "../middleware/token.js";
-import { deleteIncidenciaMiddleware } from "../middleware/deleteIncidencia.middle.js";
-import { postTrainerMiddle } from "../middleware/postTrainer.middle.js";
 const initApiRoute = () => {
     const apiRouter = Router();
     apiRouter.use('/token/:id', limitPets, tokenGeneretor)
-    apiRouter.use('/get', limitPets, tokenVerify, postTrainerMiddle, getInitRoute());
+    apiRouter.use('/get', limitPets, tokenVerify,  getInitRoute());
     apiRouter.use('/post', limitPets, limitSize, tokenVerify, postInitRoute());
     apiRouter.use('/put', limitPets, limitSize, tokenVerify, putInitRouter());
-    apiRouter.use('/delete', limitPets, tokenVerify, deleteIncidenciaMiddleware, deleteInitRoute());
+    apiRouter.use('/delete', limitPets, tokenVerify,  deleteInitRoute());
     return apiRouter;
 }
 
