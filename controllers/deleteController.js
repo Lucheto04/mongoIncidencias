e25d60d341556542091d1697266e2725256775c6
Lucheto04 <lucho.lgrg04@gmail.com>
2023-08-29 07:47:45 -0500

    refactor: :adhesive_bandage: Corregimos los codigos de respuesta

--- controllers/deleteController.js
+++ controllers/deleteController.js
@@ -4,7 +4,7 @@ export const deleteTrainerController = async (req, res) => {
     try {
         const {id} = req.query;
         const result = await deleteService.deleteTrainerService(id);
-        if(result.deletedCount === 0) return res.status(200).send("Ese trainer no existe en la base de datos");
+        if(result.deletedCount === 0) return res.status(404).send("Ese trainer no existe en la base de datos");
         if(result.deletedCount > 0) return res.status(200).send("Eliminado correctamente");
     } catch (error) {
         res.status(500).send(error);
@@ -16,7 +16,7 @@ export const deleteIncidenciaController = async (req, res) => {
         const {id} = req.query;
         const result = await deleteService.deleteIncidenciaService(id);
         if(result.deletedCount === 0) return res.status(404).send("Esa incidencia no existe en la base de datos");
-        if(result.deletedCount > 0) return res.status(404).send("Eliminado correctamente");
+        if(result.deletedCount > 0) return res.status(200).send("Eliminado correctamente");
     } catch (error) {
         res.status(500).send(error);
     }
--- controllers/putController.js
+++ controllers/putController.js
@@ -5,7 +5,7 @@ export const putTrainerController = async (req, res) => {
         const {id} = req.query;
         const data = req.body
         const result = await putService.putTrainerService(id, data);
-        if(result.modifiedCount === 0) return res.status(200).send("Ese trainer no existe en la base de datos");
+        if(result.modifiedCount === 0) return res.status(404).send("Ese trainer no existe en la base de datos");
         if(result.modifiedCount > 0) return res.status(200).send("Actualizado correctamente");
     } catch (error) {
         res.status(500).send(error);
@@ -17,7 +17,7 @@ export const putIncidenciaController = async (req, res) => {
         const {id} = req.query;
         const data = req.body;
         const result = await putService.putIncidenciaService(id, data);
-        if(result.modifiedCount === 0) return res.status(200).send("Ese trainer no existe en la base de datos");
+        if(result.modifiedCount === 0) return res.status(404).send("Ese trainer no existe en la base de datos");
         if(result.modifiedCount > 0) return res.status(200).send("Actualizado correctamente");
     } catch (error) {
         res.status(500).send(error);
